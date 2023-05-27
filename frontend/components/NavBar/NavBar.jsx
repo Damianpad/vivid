3b3890aadc437783ac046c34ff2e4bb8ac473b14
Damian Padilla <damipad4@gmail.com>
2023-05-27 00:01:42 -0500

    update: updated icons in navigation (viv-18)

--- frontend/components/NavBar/NavBar.jsx
+++ frontend/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { StyledNav } from "./NavBar.styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faRightFromBracket, faGauge, faRectangleList, faClipboardList, faCreditCard, faCalendarDays, faPeopleGroup, faBullhorn, faChartLine} from "@fortawesome/free-solid-svg-icons"
+import { faBars, faRightFromBracket, faDisplay, faUserGroup, faShapes, faDollarSign, faCalendarDays, faUsers, faBullhorn, faChartLine} from "@fortawesome/free-solid-svg-icons"
 
 const NavBar = () => {
   const [show, setShow] = useState(false)
@@ -34,44 +34,66 @@ const NavBar = () => {
       
         <div className="itemContainer">
          
-          {show? <ul>
-            <Link to='/dashboard' className="navItem">Dashboard</Link>
-            <Link to='/clients' className="navItem">Clients</Link>
-            <Link to='/projects' className="navItem">Projects</Link>
-            <Link to='/billing' className="navItem">Billing</Link>
-            <Link to='/calendar' className="navItem">Calendar</Link>
-            <Link to='/team' className="navItem">Team</Link>
-            <Link to='/marketing' className="navItem">Marketing</Link>
-            <Link to='/analytics' className="navItem">Analytics</Link>
+          {show? <ul className="itemList">
+            <Link to='/dashboard' className="navItem">
+              <FontAwesomeIcon icon={faDisplay} className="pairIcon" title="Dashboard"/>Dashboard
+              </Link>
+            <Link to='/clients' className="navItem">
+              <FontAwesomeIcon icon={faUserGroup} className="pairIcon" title="Clients"/>Clients
+              </Link>
+            <Link to='/projects' className="navItem">
+              <FontAwesomeIcon icon={faShapes} className="pairIcon" title="Projects"/>Projects
+              </Link>
+            <Link to='/billing' className="navItem">
+            <FontAwesomeIcon icon={faDollarSign} className="pairIcon" title="Billing"/>
+              Billing
+            </Link>
+            <Link to='/calendar' className="navItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="pairIcon" title="Calendar"/>Calendar
+            </Link>
+            <Link to='/team' className="navItem">
+            <FontAwesomeIcon icon={faUsers} className="pairIcon" title="Team"/>
+            Team
+            </Link>
+            <Link to='/marketing' className="navItem">
+            <FontAwesomeIcon icon={faBullhorn} className="pairIcon" title="Marketing"/>Marketing
+            </Link>
+            <Link to='/analytics' className="navItem">
+            <FontAwesomeIcon icon={faChartLine} className="pairIcon" title="Analytics"/>Analytics
+            </Link>
 
           </ul>: null}
           {!show? <ul className="iconContainer">
-            <Link to='/dashboard'><FontAwesomeIcon icon={faGauge} className="navIcon" title="Dashboard"/> </Link>
+            <Link to='/dashboard'>
+              <FontAwesomeIcon icon={faDisplay} className="navIcon" title="Dashboard"/> 
+            </Link>
 
-            <Link  to='/clients'><FontAwesomeIcon icon={faRectangleList} className="navIcon" title="Clients"/></Link>
+            <Link  to='/clients'>
+              <FontAwesomeIcon icon={faUserGroup} className="navIcon" title="Clients"/>
+            </Link>
 
             <Link to='/projects'>
-            <FontAwesomeIcon icon={faClipboardList} className="navIcon" title="Projects"/>
+              <FontAwesomeIcon icon={faShapes} className="navIcon" title="Projects"/>
             </Link>
 
             <Link to='/billing'>
-            <FontAwesomeIcon icon={faCreditCard} className="navIcon" title="Billing"/>
+              <FontAwesomeIcon icon={faDollarSign} className="navIcon" title="Billing"/>
             </Link>
             
             <Link to='/calendar'>
-            <FontAwesomeIcon icon={faCalendarDays} className="navIcon" title="Calendar"/>
+              <FontAwesomeIcon icon={faCalendarDays} className="navIcon" title="Calendar"/>
             </Link>
             
             <Link to='/team'>
-            <FontAwesomeIcon icon={faPeopleGroup} className="navIcon" title="Team"/>
+              <FontAwesomeIcon icon={faUsers} className="navIcon" title="Team"/>
             </Link>
             
             <Link to='/marketing'>
-            <FontAwesomeIcon icon={faBullhorn} className="navIcon" title="Marketing"/>
+              <FontAwesomeIcon icon={faBullhorn} className="navIcon" title="Marketing"/>
             </Link>
             
             <Link to='analytics'>
-            <FontAwesomeIcon icon={faChartLine} className="navIcon" title="Analytics"/>
+              <FontAwesomeIcon icon={faChartLine} className="navIcon" title="Analytics"/>
             </Link>
             
           </ul>: null}
